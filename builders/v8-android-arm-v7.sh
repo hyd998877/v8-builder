bedd50c041986f29edb050d9e8aa3e61b0dc7638
hyd <hyd998877@163.com>
2022-03-29 13:41:55 +0800

    Update v8-android-arm-v7.sh

--- builders/v8-android-arm-v7.sh
+++ builders/v8-android-arm-v7.sh
@@ -45,7 +45,6 @@ v8_target_cpu = "arm"
 v8_static_library = true
 is_component_build = false
 v8_monolithic = true
-use_custom_libcxx = false
 v8_enable_i18n_support = false
 v8_use_external_startup_data = false
 symbol_level = 2
