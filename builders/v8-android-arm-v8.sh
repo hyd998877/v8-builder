6d61e4a83afb4fa558b1221bae60f925de24faef
hyd <hyd998877@163.com>
2022-04-02 12:05:15 +0800

    Update v8-android-arm-v8.sh

--- builders/v8-android-arm-v8.sh
+++ builders/v8-android-arm-v8.sh
@@ -40,18 +40,41 @@ gclient sync
 echo "=====[ Building V8 ]====="
 python ./tools/dev/v8gen.py arm64.release -vv -- '
 target_os = "android"
-target_cpu = "arm"
-v8_target_cpu = "arm"
+target_cpu = "arm64"
+v8_target_cpu = "arm64"
+is_debug = false
 v8_static_library = true
 is_component_build = false
 v8_monolithic = true
-use_custom_libcxx = true
 v8_enable_i18n_support = false
 v8_use_external_startup_data = false
-symbol_level = 2
-strip_debug_info = false
+symbol_level = 0
+strip_debug_info = true
 v8_enable_webassembly = false
 v8_enable_wasm_gdb_remote_debugging = false
+use_custom_libcxx = false
+v8_use_snapshot = false
+v8_enable_disassembler = false
+v8_enable_gdbjit = false
+v8_enable_handle_zapping = false
+v8_no_inline = true
+v8_experimental_extra_library_files = []
+v8_extra_library_files = []
+v8_enable_concurrent_marking = true
+v8_enable_embedded_builtins = false
+is_desktop_linux = false
+icu_use_data_file = false
+enable_iterator_debugging = false
+enable_precompiled_headers = false
+clang_use_chrome_plugins = false
+asan_globals = false
+libcpp_is_static = true
+use_aura = false
+use_dbus = false
+use_gio = false
+use_glib = false
+use_icf = false
+use_udev = false
 '
 ninja -C out.gn/arm64.release -t clean
 ninja -C out.gn/arm64.release v8_libplatform
