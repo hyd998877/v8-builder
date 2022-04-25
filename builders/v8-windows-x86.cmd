99865cfcb34d5bdb264327708d286bb91306e887
hyd <hyd998877@163.com>
2022-04-26 01:48:28 +0800

    Update v8-windows-x86.cmd

--- builders/v8-windows-x86.cmd
+++ builders/v8-windows-x86.cmd
@@ -28,7 +28,7 @@ call git apply --ignore-whitespace --verbose %GITHUB_WORKSPACE%\builders\BUILD.g
 
 
 echo =====[ Building V8 ]=====
-call python .\tools\dev\v8gen.py ia32.release -vv -- target_os="""win""" is_component_build=true use_custom_libcxx=false is_clang=true use_lld=false v8_enable_verify_heap=false v8_enable_i18n_support=true v8_use_external_startup_data=false symbol_level=0 target_cpu="""x86""" v8_target_cpu="""x86"""
-
+#call python .\tools\dev\v8gen.py ia32.release -vv -- target_os="""win""" is_component_build=true use_custom_libcxx=false is_clang=true use_lld=false v8_enable_verify_heap=false v8_enable_i18n_support=true v8_use_external_startup_data=false symbol_level=0 target_cpu="""x86""" v8_target_cpu="""x86"""
+python .\tools\dev\v8gen.py ia32.release -- v8_monolithic=true v8_enable_i18n_support=false v8_use_external_startup_data=false use_custom_libcxx=false is_component_build=false treat_warnings_as_errors=false v8_symbol_level=0
 call ninja -C out.gn\ia32.release -t clean
-call ninja -C out.gn\ia32.release v8
+call ninja -C out.gn\ia32.release v8_monolith
